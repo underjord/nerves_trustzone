b6217440515b4ee648afe104863a71e43ec57999
Lars Wikman <lars@underjord.io>
2024-05-29 16:13:07 +0200

    Add common test-commands

--- lib/nerves_trustzone.ex
+++ lib/nerves_trustzone.ex
@@ -1,4 +1,5 @@
 defmodule NervesTrustzone do
+  @library_path "/usr/lib/libckteec.so.1.1.0"
   def child_spec(_opts) do
     opts = []
 
@@ -6,5 +7,17 @@ defmodule NervesTrustzone do
       id: __MODULE__,
       start: {MuonTrap.Daemon, ["tee-supplicant", [], opts]}
     }
+  end
+
+  def test_trustzone do
+    MuonTrap.cmd("xtest", [])
+  end
+
+  def pkcs11_tool_info do
+    MuonTrap.cmd("pkcs11-tool", ["--module", "/usr/lib/libckteec.so", "--show-info"])
+  end
+
+  def pkcs11_tool_slots do
+    MuonTrap.cmd("pkcs11-tool", ["--module", "/usr/lib/libckteec.so", "--show-info"])
   end
 end
